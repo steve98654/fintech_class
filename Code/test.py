10b3473fd1292a898d0071317b61ec16480cadf1
Zongqi Shen7 <derrickshen77@gmail.com>
2022-10-05 20:44:29 -0400

    Second Upload

--- Code/test.py
+++ Code/test.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import matplotlib.pyplot as plt
 
 data = np.random.randn(100) 
 mn = np.mean(data)
@@ -6,4 +7,5 @@ std = np.std(data)
 print(np.cumsum(data)[-1])
 print(mn)
 print(std)
-
+plt.plot(data)
+plt.show()
